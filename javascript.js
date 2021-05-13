a48d87ad8d71bec2c6fe835c602625f740933923
Jose G. Basanta <jgj1989@gmail.com>
2021-05-13 12:05:32 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,22 +1,17 @@
-// las tres posibles jugadas en piedra papel y tijera.
 const options = ["rock", "paper", "scissors"];
 
-//Funcion para que la computadora elija su jugada y la refleje en la consola
 function computerPlay() {
     const random = Math.floor(Math.random() * options.length);
     return options[random];
 };
 
-console.log(computerPlay());
-
-//funcion para una sola partida entre el usuario y la computadora
 function playRound(playerSelection, computerSelection) {
     if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock")) {
         return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection == computerSelection) {
         return "Its a tie! Play Again";
     } else if (playerSelection !== "rock" || "paper" || "scissors") {
-        return "Please select Rock, Paper or Scissors.hola"
+        return "Please select Rock, Paper or Scissors."
     } else {
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     };
